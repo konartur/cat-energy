7300d45c656c450b683f1a9ffb31dc52a17e3766
suntikflood <suntikflood@gmail.com>
2020-07-16 16:07:12 +0300

    add sourcemaps and gulp-if

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
+const sourcemaps = require("gulp-sourcemaps");
+const gulpIf = require("gulp-if");
+
+const isDevelopment =
+  !process.env.NODE_ENV || process.env.NODE_ENV == "development";
 
 gulp.task("styles", function () {
-  return gulp.src("src/**/*.scss").pipe(sass()).pipe(gulp.dest("public"));
+  return gulp
+    .src("src/styles/main.scss")
+    .pipe(gulpIf(isDevelopment, sourcemaps.init()))
+    .pipe(sass())
+    .pipe(gulpIf(isDevelopment, sourcemaps.write(".")))
+    .pipe(gulp.dest("public"));
 });
