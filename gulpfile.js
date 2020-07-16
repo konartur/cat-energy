d3d972b03b60eb49fa73a5eb168cd89125fdb3a9
suntikflood <suntikflood@gmail.com>
2020-07-16 16:33:44 +0300

    create build task

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,26 @@ const gulp = require("gulp");
 const sass = require("gulp-sass");
 const sourcemaps = require("gulp-sourcemaps");
 const gulpIf = require("gulp-if");
+const del = require("del");
 
 const isDevelopment =
   !process.env.NODE_ENV || process.env.NODE_ENV == "development";
 
 gulp.task("styles", function () {
   return gulp
-    .src("src/styles/main.scss")
+    .src("assets/styles/main.scss")
     .pipe(gulpIf(isDevelopment, sourcemaps.init()))
     .pipe(sass())
     .pipe(gulpIf(isDevelopment, sourcemaps.write(".")))
     .pipe(gulp.dest("public"));
 });
+
+gulp.task("clean", function () {
+  return del("public");
+});
+
+gulp.task("assets", function () {
+  return gulp.src("assets/**").pipe(gulp.dest("public"));
+});
+
+gulp.task("build", gulp.series("clean", gulp.parallel("styles", "assets")));
