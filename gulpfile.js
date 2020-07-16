e8a3c9ae35c60bf60230fbe9659e8d8088808e2e
suntikflood <suntikflood@gmail.com>
2020-07-16 17:27:44 +0300

    add watch

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,14 @@ const sass = require("gulp-sass");
 const sourcemaps = require("gulp-sourcemaps");
 const gulpIf = require("gulp-if");
 const del = require("del");
+const newer = require("gulp-newer");
 
 const isDevelopment =
   !process.env.NODE_ENV || process.env.NODE_ENV == "development";
 
 gulp.task("styles", function () {
   return gulp
-    .src("assets/styles/main.scss")
+    .src("styles/main.scss")
     .pipe(gulpIf(isDevelopment, sourcemaps.init()))
     .pipe(sass())
     .pipe(gulpIf(isDevelopment, sourcemaps.write(".")))
@@ -21,7 +22,17 @@ gulp.task("clean", function () {
 });
 
 gulp.task("assets", function () {
-  return gulp.src("assets/**").pipe(gulp.dest("public"));
+  return gulp
+    .src("assets/**", { since: gulp.lastRun("assets") })
+    .pipe(newer("public"))
+    .pipe(gulp.dest("public"));
 });
 
 gulp.task("build", gulp.series("clean", gulp.parallel("styles", "assets")));
+
+gulp.task("watch", function () {
+  gulp.watch("styles/**/*.*", gulp.series("styles"));
+  gulp.watch("assets/**/*.*", gulp.series("assets"));
+});
+
+gulp.task("dev", gulp.series("build", "watch"));
