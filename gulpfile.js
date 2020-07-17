0a9ad6f37b4e79f8ae277e60966048e1682bb56a
suntikflood <suntikflood@gmail.com>
2020-07-17 22:30:22 +0300

    complete gulpfiles

--- gulpfile.js
+++ gulpfile.js
@@ -3,65 +3,75 @@ const sass = require("gulp-sass");
 const sourcemaps = require("gulp-sourcemaps");
 const gulpIf = require("gulp-if");
 const del = require("del");
-const newer = require("gulp-newer");
-const autoprefixer = require("gulp-autoprefixer");
-const remember = require("gulp-remember");
+const autoprefixer = require("autoprefixer");
 const browserSync = require("browser-sync").create();
 var pug = require("gulp-pug");
+const imagemin = require("gulp-imagemin");
+const postcss = require("gulp-postcss");
+const rename = require("gulp-rename");
+const cssnano = require("cssnano");
+var ghpages = require("gh-pages");
 
-var dir = "public";
+var dist = "public";
 
 const isDevelopment =
   !process.env.NODE_ENV || process.env.NODE_ENV == "development";
 
 gulp.task("styles", function () {
   return gulp
-    .src("styles/main.scss")
-    .pipe(autoprefixer())
-    .pipe(remember("styles"))
+    .src("src/styles/pages/*", { since: gulp.lastRun("styles") })
     .pipe(gulpIf(isDevelopment, sourcemaps.init()))
     .pipe(sass())
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(rename({ suffix: ".min" }))
     .pipe(gulpIf(isDevelopment, sourcemaps.write(".")))
-    .pipe(gulp.dest(dir));
+    .pipe(gulp.dest(dist))
+    .pipe(browserSync.stream());
 });
 
 gulp.task("pug", function () {
   return gulp
-    .src("assets/templates/**/*.pug")
-    .pipe(
-      pug({
-        // Your options in here.
-      })
-    )
-    .pipe(gulp.dest("public/html"))
+    .src("src/views/pages/**/*.pug", { since: gulp.lastRun("pug") })
+    .pipe(pug())
+    .pipe(gulp.dest(dist))
     .pipe(browserSync.reload({ stream: true }));
 });
 
-gulp.task("clean", function () {
-  return del(dir);
-});
-
-gulp.task("assets", function () {
+gulp.task("images", function () {
   return gulp
-    .src("assets/**", { since: gulp.lastRun("assets") })
-    .pipe(newer(dir))
-    .pipe(gulp.dest(dir));
+    .src("src/images/*", { since: gulp.lastRun("images") })
+    .pipe(imagemin())
+    .pipe(gulp.dest(dist));
 });
 
-gulp.task("build", gulp.series("clean", gulp.parallel("styles", "assets")));
+gulp.task("git-publish", function (cb) {
+  ghpages.publish(dist);
+  cb();
+});
+
+gulp.task("clean", function () {
+  return del(dist);
+});
+
+gulp.task(
+  "build",
+  gulp.series("clean", gulp.parallel("styles", "images", "pug"))
+);
+
+gulp.task("deploy", gulp.series("build", "git-publish"));
 
 gulp.task("watch", function () {
-  gulp.watch("styles/**/*.*", gulp.series("styles"));
-  // gulp.watch("assets/**/*.pug", ["pug"]);
-  gulp.watch("assets/**/*.*", gulp.series("assets"));
+  gulp.watch("src/styles/**/*.scss", gulp.series("styles"));
+  gulp.watch("src/views/**/*.pug", gulp.series("pug"));
+  gulp.watch("src/images/*", gulp.series("images"));
 });
 
 gulp.task("server", function () {
   browserSync.init({
-    server: dir,
+    server: dist,
   });
 
-  browserSync.watch("public/**/*.*").on("change", browserSync.reload);
+  browserSync.watch(dist + "/**/*.*").on("change", browserSync.reload);
 });
 
-gulp.task("dev", gulp.series("build", gulp.parallel("watch", "server")));
+gulp.task("default", gulp.series("build", gulp.parallel("watch", "server")));
